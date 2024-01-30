4dce3840555c4ede906efa4cf6b13bbb74f86fe6
Akashsushil <akashsushil1999@gmail.com>
2024-01-30 12:22:33 +0530

    final

---
+++ task - Copy.py
@@ -0,0 +1,26 @@
+a="handsome"
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(len(a))
+print(a.split(","))
+print(a.replace("h","w"))
+print(a[1:4])
+b="akash"
+c=a+" "+b
+print(c)
+x,y,z="hy","hello","bye"
+print(x)
+print(y)
+print(z)
+d=e=f="hello"
+print(d)
+dogs=["corgi","chow chow","bullmastiff"]
+x,y,z="corgi","chow chow","bullmastiff"
+print(x)
+print(y)
+print(z)
+akash="i love my country india is my country"
+print(akash.count("country"))
+
