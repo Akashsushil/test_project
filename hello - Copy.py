4f7c7b5792b71f6414587f44dad739f5d2ee0a20
Akashsushil <akashsushil1999@gmail.com>
2024-01-30 12:12:51 +0530

    success

---
+++ hello - Copy.py
@@ -0,0 +1,44 @@
+print("hello akash") #print
+a=10
+print(a)
+print(10)
+x,y,z="apple","orange","cherry"
+print(x)
+print(y)
+print(z)
+x=y=z="apple"
+print(z)
+fruits=["apple","orange","grapes"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+b=6.5
+print(int(b))
+c=2
+d=3+8j
+print(float(c))
+print(type(d))
+import random
+print(random.randrange(1,100))
+print("akash")
+e="  palin\drome"
+print(e)
+print(e[2:5])
+print(len(e))
+print(e.lower())
+print(e.strip())
+print(e.replace("p","v"))
+f="my"
+g="name"
+h=f+"  "+g
+print(h)
+
+
+
+
+
+
+
+
+      
