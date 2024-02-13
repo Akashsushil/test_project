4f28ad7532b5429ccfef97ddb06efcb28ac9868d
Akashsushil <97270855+Akashsushil@users.noreply.github.com>
2024-02-13 09:12:37 +0530

    Add files via upload

---
+++ taskpattern.py
@@ -0,0 +1,13 @@
+#while loop to print pattern
+rows=4
+for i in range(1,rows+1):
+    for j in range(i):
+        print((2*i-1),end="  ")
+    print()    
+
+
+    #reverse pattern
+    for num in range(22,0,-1):
+        print(num)
+
+
